203227232af9b936d8a932389df561206348c518
bevinh <bevin.hernandez@gmail.com>
2017-05-25 09:47:13 -0400

    added additional ingredients storage

--- public/scripts/recipeDetailController.js
+++ public/scripts/recipeDetailController.js
@@ -1,6 +1,7 @@
 angular.module('app')
 
-    .controller('recipeDetailController', function(dataService, $scope){
+    .controller('recipeDetailController', function(dataService, $scope, $compile){
+        $scope.recipe = {ingredients: [{name: '', condition: '', amount: ''}]}
         //gets categories for dropdown
         dataService.getCategories(function(response){
             $scope.categories = response.data;
@@ -8,9 +9,21 @@ angular.module('app')
         //gets foodItems for dropdown
         dataService.getFoodItems(function(response){
             $scope.foodItems = response.data;
-        })
+        });
         $scope.saveRecipe = function(recipe){
             console.log(recipe)
             //dataService.addRecipe(recipe);
-        }
+        };
+        $scope.addRecipeItem = function() {
+                 $scope.recipe.ingredients.push({name: '', condition: '', amount: '' })
+        };
+        $scope.removeRecipeIngredient = function(index, recipe){
+            var ingredient = recipe.ingredients[index];
+            if(ingredient.id){
+                ingredient._destroy = true;
+            }else{
+                recipe.ingredients.splice(index, 1);
+            }
+        };
+
     });
