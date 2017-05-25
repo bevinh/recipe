2bb91ad421123b5a4d3d36be652d7215af75eadd
bevinh <bevin.hernandez@gmail.com>
2017-05-25 09:53:14 -0400

    added steps storage

--- public/scripts/recipeDetailController.js
+++ public/scripts/recipeDetailController.js
@@ -1,7 +1,7 @@
 angular.module('app')
 
     .controller('recipeDetailController', function(dataService, $scope, $compile){
-        $scope.recipe = {ingredients: [{name: '', condition: '', amount: ''}]}
+        $scope.recipe = {ingredients: [{name: '', condition: '', amount: ''}], steps: [{description: ''}]};
         //gets categories for dropdown
         dataService.getCategories(function(response){
             $scope.categories = response.data;
@@ -17,12 +17,26 @@ angular.module('app')
         $scope.addRecipeItem = function() {
                  $scope.recipe.ingredients.push({name: '', condition: '', amount: '' })
         };
+
+        $scope.addStep = function() {
+            $scope.recipe.steps.push({description: '' })
+        };
+
         $scope.removeRecipeIngredient = function(index, recipe){
             var ingredient = recipe.ingredients[index];
             if(ingredient.id){
                 ingredient._destroy = true;
             }else{
                 recipe.ingredients.splice(index, 1);
+            }
+        };
+
+        $scope.removeStep = function(index, recipe){
+            var step = recipe.steps[index];
+            if(step.id){
+                step._destroy = true;
+            }else{
+                recipe.steps.splice(index, 1);
             }
         };
 
